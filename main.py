cfe02f63670aafd40584ff594577f5ee559eeb56
Plcuf <romeo.sanchez@ynov.com>
2024-09-24 09:00:49 +0200

    fix: merge

--- main.py
+++ main.py
@@ -13,10 +13,14 @@ dt = 0
 player1_position = pygame.Vector2(25,screen.get_height() / 2 - 50)
 player2_position = pygame.Vector2(screen.get_width() - 50,screen.get_height() / 2 - 50)
 
+centered_player1_position = pygame.Vector2(player1_position.x + 25, player1_position.y + 50)
+centered_player2_position = pygame.Vector2(player2_position.x, player2_position.y + 50)
+
 ball_position = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
-ball_direction = pygame.Vector2(random.randint(-100, 100), random.randint(-100, 100))
-ball_direction = ball_direction.normalize()
-ball_speed = 5
+#ball_direction = pygame.Vector2(random.randint(-100, 100), random.randint(-100, 100))
+#ball_direction = ball_direction.normalize()
+ball_direction = pygame.Vector2(1, 0)
+ball_speed = 8
 
 while running:
     # poll for events
@@ -28,14 +32,29 @@ while running:
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("purple")
 
+    # draw the shapes :D
     pygame.draw.rect(screen, "white", (player1_position.x, player1_position.y, 25, 100))
     pygame.draw.rect(screen, "white", (player2_position.x, player2_position.y, 25, 100))
     pygame.draw.circle(screen, "white", (ball_position.x, ball_position.y), 15)
 
+    # center the player positions
+    centered_player1_position = pygame.Vector2(player1_position.x + 12.5, player1_position.y + 50)
+    centered_player2_position = pygame.Vector2(player2_position.x + 12.5, player2_position.y + 50)
+
+    #update ball position
     ball_position += ball_direction * ball_speed
 
+    # bounce the ball on top and bottom side
     if (ball_position.y - 15) <= 0 or (ball_position.y + 15) >= screen.get_height():
         ball_direction.y = -ball_direction.y
+
+    # bounce the ball on player 1
+    if ball_position.x - player1_position.x <= 10 and ball_position.y - player1_position.y <= 50:
+        ball_direction.x = -ball_direction.x
+
+    # bounce the ball on player 2
+    if ball_position.x - player2_position.x >= 0 and ball_position.y - player2_position.y <= 50:
+        ball_direction.x = -ball_direction.x
 
     keys = pygame.key.get_pressed()
 
